cd8c978988717dfffa89b4eedb6316d1d4986383
ohyeong <oueong@naver.com>
2022-08-05 23:59:21 +0900

    bottom navigation

---
+++ app/src/main/java/com/example/flosfiore/MapFragment.kt
@@ -0,0 +1,24 @@
+package com.example.flosfiore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flosfiore.databinding.FragmentMapBinding
+
+// 지도 프레그먼트
+class MapFragment : Fragment() {
+
+    lateinit var binding: FragmentMapBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMapBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
